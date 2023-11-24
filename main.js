e27876b3da93dda7fa631e4ef8a00bbb91fe502f
Andrew <Andrey_U_O@mail.ru>
2023-11-24 18:54:20 +0300

    redir fix

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 btns = document.getElementsByClassName("headBtn");
 logBtn = btns[0];
 regBtn = btns[1];
-logBtn.addEventListener("click", () => {
-  window.location.href = "http://localhost:63342/JsPractice/reg/index.html";
-});
 currentURL = window.location.href;
-alert("current URL: " + currentURL);
+
+logBtn.addEventListener("click", () => {
+  window.location.href = `${currentURL}login`;
+});
 regBtn.addEventListener("click", () => {
-  window.location.href = `${currentURL}/reg/index.html`;
+  window.location.href = `${currentURL}reg`;
 });
